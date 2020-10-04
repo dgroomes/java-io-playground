789733ee0ecb6bf3773df1c1956fb14f498f7ae2
David Groomes <dwgroomes@gmail.com>
2020-10-04 17:27:23 -0500

    Scaffold 'java-io-playground'

---
+++ README.md
@@ -0,0 +1,15 @@
+# java-io-playground
+
+NOT YET IMPLEMENTED
+
+📚 Learning and exploring Java's IO (input/output) standard libraries.
+
+---
+
+### Instructions
+
+NOTE: this was developed on macOS.
+
+1. Use Java 15
+1. Execute `./build.sh` to compile the source code 
+1. Execute `./run.sh` to run the program (i.e. the `main` method).
---
+++ src/dgroomes/Main.java
@@ -0,0 +1,16 @@
+package dgroomes;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * See the README.md for more information.
+ */
+public class Main {
+
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) throws Exception {
+        log.info("Hello world!");
+    }
+}

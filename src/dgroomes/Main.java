a088d3683e0808c884e6b06d2149993710f2d3e4
David Groomes <dwgroomes@gmail.com>
2020-10-04 22:02:56 -0500

    read files using BufferedReader

--- src/dgroomes/Main.java
+++ src/dgroomes/Main.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.impl.SimpleLogger;
 
+import java.io.*;
+
 /**
  * See the README.md for more information.
  */
@@ -19,8 +21,29 @@ public class Main {
     }
 
     public static void main(String[] args) throws Exception {
-        log.info("Hello world!");
+        var file = new File("tmp/large-temp-file.txt");
+        log.info("Reading lines from  the large temp file");
+        int linesRead = readLines_bufferedReader(file);
+        log.info("Read {} lines from the large temp file", linesRead);
+    }
 
+    /**
+     * Read from file using {@link BufferedReader} and {@link FileReader}
+     * @param file
+     * @return
+     * @throws IOException
+     */
+    private static int readLines_bufferedReader(File file) throws IOException {
+        var reader = new BufferedReader(new FileReader(file));
+        int linesRead = 0;
+        String line;
+        while ((line = reader.readLine()) != null) {
+            linesRead++;
+        }
+        return linesRead;
+    }
+
+    private static void generateTestFile() {
         var dataGenerator = new TestDataGenerator();
         var generatedFile = dataGenerator.generateLargeFile();
         log.debug("Generated a large file to {}", generatedFile.getAbsolutePath());
